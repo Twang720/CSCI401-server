e5ed3ddfc1f86935d1b670649929b94115c25a86
Timmy Wang <tqwang@usc.edu>
2022-12-02 17:18:00 -0800

    Articles create

---
+++ models/articles.js
@@ -0,0 +1,11 @@
+class Article {
+    constructor(id, firstName, lastName, email, password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+module.exports = Article;
